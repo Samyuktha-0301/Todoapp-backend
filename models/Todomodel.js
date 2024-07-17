d99320bfeebf4e6b1001debb4d4eb195b24a7361
Samyuktha-0301 <103641209+Samyuktha-0301@users.noreply.github.com>
2024-07-17 22:08:33 +0530

    Update Todomodel.js

--- models/Todomodel.js
+++ models/Todomodel.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const todoSchema = new mongoose.Schema({
     text:{
         type:String,
-        require:true}
+        require:true},
+    user:[{
+            type:mongoose.Types.ObjectId,
+            ref : "USER"},
+    ]
     })
     
 module.exports = mongoose.model('ToDo', todoSchema)
