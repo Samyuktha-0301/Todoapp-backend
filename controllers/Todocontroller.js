46de8626ef2c169e86a86d818af1290bb38aa4c4
Samyuktha-0301 <103641209+Samyuktha-0301@users.noreply.github.com>
2024-07-17 22:08:12 +0530

    Update Todocontroller.js

--- controllers/Todocontroller.js
+++ controllers/Todocontroller.js
@@ -1,18 +1,40 @@
 const ToDoModel = require('../models/Todomodel');
+const User=require("../models/user");
 
 module.exports.getTodo = async (req, res) => {
-    const toDo = await ToDoModel.find();
+    const { _id } = req.body;
+        // Ensure user exists
+        const existingUser = await User.findById(_id);
+        if (existingUser) {
+            // Find all to-dos related to the user
+            const todos = await ToDoModel.find({ user: _id });
+
+            res.json({ list: todos });
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
    
-    res.send(toDo);
 };
 
 module.exports.saveToDo = async (req, res) => {
     try {
-        const  {text} = req.body; 
-        const resP = await ToDoModel.create({ text }); // Pass the extracted data as text
-        console.log("Added Successfully...");
-        console.log(resP);
-        res.send(resP);
+        
+            const { text, email } = req.body;
+            const existingUser = await User.findOne({ email });
+    
+            if (existingUser) {
+                const list = new ToDoModel({ text, user: existingUser._id });
+                await list.save();
+    
+                existingUser.list.push(list._id);
+                await existingUser.save();
+    
+                res.status(200).json({ list });
+            } else {
+                res.status(404).json({ message: "User not found" });
+            }
+        
+        
     } catch (err) {
         console.log(err);
         res.status(500).send(err); // Send an error response in case of failure
@@ -20,9 +42,20 @@ module.exports.saveToDo = async (req, res) => {
 };
 module.exports.updateToDo= async(req,res)=>{
     try{
-        const { _id ,text}=req.body;
-        const resP=await ToDoModel.findByIdAndUpdate(_id,{text});
-        res.send("Updated Successfully");
+        const { _id ,text,email}=req.body;
+        const existingUser = await User.findOne({ email });
+        
+            if (existingUser) {
+                const updatedToDo =await ToDoModel.findByIdAndUpdate(_id,{ text});
+                if (updatedToDo) {
+                    res.status(200).json({ message: "Updated Successfully", updatedToDo });
+                } else {
+                    res.status(404).json({ message: "ToDo not found for this user" });
+                }
+                
+            } else {
+                res.status(404).json({ message: "User not found" });
+            }
 
     }
     catch(err){
@@ -32,26 +65,20 @@ module.exports.updateToDo= async(req,res)=>{
 };
 module.exports.deleteToDo = async (req, res) => {
     try {
-        
-        const  {_id}  = req.body;
-        console.log(_id);
-        await ToDoModel.findByIdAndDelete(_id);
-        res.send("Deleted Successfully");
+        const { _id, email } = req.query;
+        const existingUser = await User.findOneAndUpdate(
+            { email },
+            { $pull: { list: _id } }
+        );
+
+        if (existingUser) {
+            await ToDoModel.findByIdAndDelete(_id);
+            res.send("Deleted Successfully");
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
     } catch (err) {
-        console.log( err);
+        console.log(err);
         res.status(500).send(err);
     }
 };
-// module.exports.deleteToDo= async(req,res)=>{
-//     try{
-//         const {_id}=req.body;
-//         await ToDoModel.findByIdAndDelete(_id);
-//         res.send("DeletedSuccessfully");
-
-//     }
-//     catch(err){
-//         console.log(err);
-//         res.status(500).send(err);
-//     }
-// };
-
