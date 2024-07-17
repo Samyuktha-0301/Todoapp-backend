a13ff266f66a79b8b14681bdb9550b7e1eca658f
Samyuktha-0301 <103641209+Samyuktha-0301@users.noreply.github.com>
2024-07-17 22:10:23 +0530

    Update Todoroute.js

--- routes/Todoroute.js
+++ routes/Todoroute.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { saveToDo, getTodo, updateToDo, deleteToDo } = require('../controllers/Todocontroller');
 const router = Router();
 
-router.get('/', getTodo);
+router.post('/', getTodo);
 router.post('/save', saveToDo);
 router.put('/update', updateToDo); // Changed POST to PUT for update
 router.delete('/delete', deleteToDo); // Ensure the route is correct for DELETE
